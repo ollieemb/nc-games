febde793680b5bd8df2583f41a9c1689628cdef7
Ollie Embley <ollieembley@MacBook-Air.local>
2023-03-15 12:14:47 +0000

    working votes- needs tinkering

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
-import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { getReviews } from '../utils/api';
+import ReviewCard from './ReviewCard';
 
 function Home() {
   const [reviews, setReviews] = useState([]);
@@ -10,35 +10,13 @@ function Home() {
       setReviews(reviewsData);
     }) 
   }, [])
-
+  
+  
   return(
     <>
       <ul className="card-container">
         {reviews.map((review) => {
-          return(
-            <li className='card' key={review.review_id}>
-              <img 
-                src={review.review_img_url}
-                alt={review.title}
-                className="pictureCard"
-              />
-              <h2> Title: {review.title}</h2>
-              <p>Designer: {review.designer}</p>
-              <p> Owner: {review.owner}</p>
-              <p>Votes: {review.votes}</p>
-              <p>Category: {review.category}</p>
-              <p>Created At: {review.created_at}</p>
-              <p>Comment Count: {review.comment_count}</p>
-              <button>Upvote</button>
-              <button>Downvote</button>
-              <br/>
-              <br/>
-              <Link to={`/reviews/${review.review_id}`}>View Review and Comments</Link>
-              <br/>
-              <br/>
-              <br/>
-            </li>
-          )
+          return <ReviewCard review={review} />
         })}
       </ul>
     </>
---
+++ src/components/ReviewCard.jsx
@@ -0,0 +1,58 @@
+import { Link } from 'react-router-dom';
+import { voteOnReviewHome } from '../utils/api';
+import {  useState } from 'react';
+
+
+function ReviewCard ({review}) {
+    const [isVotingError, setIsVotingError] = useState(false);
+      const [userVote, setUserVote] = useState(0);
+
+
+    const onClick = (review_id) => {
+        setUserVote(+1)
+        
+        setIsVotingError(false);
+        voteOnReviewHome(review_id)
+          
+          .catch(() => {
+            setIsVotingError(true);
+            setUserVote(0)
+          });
+      };
+    
+    return(
+        <li className='card' key={review.review_id}>
+          <img 
+            src={review.review_img_url}
+            alt={review.title}
+            className="pictureCard"
+          />
+          <h2> Title: {review.title}</h2>
+          <p>Designer: {review.designer}</p>
+          <p> Owner: {review.owner}</p>
+          <p>Category: {review.category}</p>
+          <p>Created At: {review.created_at}</p>
+          <p>Comment Count: {review.comment_count}</p>
+          <p>Votes: {review.votes + userVote}</p>
+          <button
+type="button"
+onClick={() => onClick(review.review_id)}
+disabled={userVote !== 0}
+>
+Upvote
+</button>
+            {isVotingError && <p> vote not registered </p>}
+
+         
+          <br/>
+          <br/>
+          <Link to={`/reviews/${review.review_id}`}>View Review and Comments</Link>
+          <br/>
+          <br/>
+          <br/>
+        </li>
+      )
+
+}
+
+export default ReviewCard
